049ec6e899a14f485633316e3b2f925665de1caf
Dawson Ley <daw5@Dawsons-MacBook-Pro-2.local>
2025-05-20 19:15:45 -0700

    static type checking added with no errors, now to add types where necessary

--- remotereps/settings.py
+++ remotereps/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from typing import List
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-mnd=h%fb6^h_gesola+ye4w%4b6v%^654ab#0pyyzx9cbkx32h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: List[str] = []
 
 
 # Application definition
